7fede7ab0355fedfe0e4c536bfc4616d13f9340c
Rahul R M <88234236+CryptoMaN-Rahul@users.noreply.github.com>
2023-10-11 19:48:18 +0530

    Update pda.py

--- pda.py
+++ pda.py
@@ -1,16 +1,15 @@
 nstate = int(input("Enter the number of states: "))
-ntransition = int(input("Enter the number of transition states: "))
+ntransition = int(input("Enter the number of transition state: "))
 pda = {}
 stack = []
 print("state input top stack new_state")
-
 for i in range(ntransition):
-    transition = input().split(" ")
-    temp = []
-    try:
-        pda[transition[0]].append(transition[1:])
-    except KeyError:
-        pda[transition[0]] = [transition[1:]]
+  transition = input().split(" ")
+  temp = []
+  try:
+    pda[transition[0]].append(transition[1:]) 
+  except KeyError:
+    pda[transition[0]] = [transition[1:]]
 
 input_string = input("Enter the input string")
 final_state = input("Enter the final state")
@@ -18,31 +17,27 @@ print(pda)
 
 state = '0'
 for i in input_string:
-    print(i, end=" - ")
-    trans = pda[state]
-    for b in trans:
-        if b[2] == "^":
-            stack.pop()
-        print(" | ".join(n for n in b))
-        state = b[3]
-        break
+  print(i,end = " - ")
+  trans = pda[state]
+  for b in trans:
+    if b[2] == "^":
+      stack.pop()
+    print(" | ".join(n for n in b))  
+    state = b[3]
+    break
+  if len(stack) == 0 and b[0] == '^':
+    state = b[3]
+    break
+  if i == b[0]:
+    stack.append(b[1])
+    print(" | ".join(n for n in b))
+    state = b[3]
+    break
+  if len(stack) == 0 or b[1] != stack[len(stack)-1]:
+    continue
 
-    if len(stack) == 0 and b[0] == '^':
-        state = b[3]
-        break
-
-    if i == b[0]:
-        stack.append(b[1])
-        print(" | ".join(n for n in b))
-        state = b[3]
-        break
-
-    if len(stack) == 0 or b[1] != stack[len(stack) - 1]:
-        continue
-
-    print("state is " + state)
-
+print("state is "+state)
 if state == final_state or len(stack) == 0:
-    print("accepted")
+  print("accepted")
 else:
-    print("not accepted")
+  print("not accepted")

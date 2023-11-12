f239b07d4163098050d40bc2e8cdf58d21526086
Rahul R M <88234236+CryptoMaN-Rahul@users.noreply.github.com>
2023-11-12 13:00:32 +0530

    Update mealy easy.py

--- mealy easy.py
+++ mealy easy.py
@@ -74,3 +74,54 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+'''
+Create a MealyMachine instance with an empty set for states, inputs, and an empty dictionary for transitions.
+
+Add States, Inputs, and Transitions:
+2. Take user input for the number of states (num_states).
+3. For each state:
+
+Take user input for the state name (state).
+Add the state to the Mealy Machine using add_state.
+
+
+
+Take user input for the number of input symbols (num_inputs).
+
+
+For each input symbol:
+
+Take user input for the input symbol (input_symbol).
+Add the input symbol to the Mealy Machine using add_input.
+
+
+
+For each state in the Mealy Machine:
+
+Take user input for the current state (current_state).
+Take user input for the number of transitions for that state (num_transitions).
+For each transition:
+
+Take user input for the input symbol for this transition (input_symbol).
+Take user input for the next state (next_state).
+Take user input for the output symbol for this transition (output_symbol).
+Add the transition using add_transition.
+
+
+Set Initial State:
+7. Take user input for the initial state (initial_state).
+8. Set the initial state using set_initial_state.
+Process Input String:
+9. Take user input for the input string (input_string).
+10. Initialize an empty string for output_sequence.
+Main Loop:
+11. For each symbol in the input string:
+- Perform the transition using transition and update the current state.
+- Accumulate the output in the output_sequence.
+- Print the input symbol, current state, and output.
+Print Output:
+12. Print the final output_sequence.
+'''
